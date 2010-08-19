138ac068fbcc4263f3b37e08ef8984167e0e96cd
Lydon Chandra <lydonchandra@mini-2.local>
2010-08-20 00:29:57 +0800

    first commit

---
+++ src/main/java/com/don/activequant0/App.java
@@ -0,0 +1,13 @@
+package com.don.activequant0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
